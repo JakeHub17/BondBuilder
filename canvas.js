e6cb13e2a62f8ba3b612934be0dec6ad4a5dfd97
Jacob Mastrandrea <williamjacob174@gmail.com>
2023-04-30 13:48:56 -0400

    Adding the drawing function to allows a simple line to be drawn

--- canvas.js
+++ canvas.js
@@ -1,3 +1,36 @@
-var canvas = document.querySelector('canvas');
+window.addEventListener('load' , ()=> {
+  const canvas = document.querySelector("#canvas");
+  const ctx = canvas.getContext("2d");
 
+  //resizing
+  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+
+  //painting
+
+  let painting = false;
+
+  function startPosition(){
+    painting = true;
+  }
+
+  function finishedPosition(){
+    painting = false;
+  }
+
+  function draw(e){
+    if(!painting) return;
+    ctx.lineWidth = 1;
+    ctx.lineCap = "round";
+
+    ctx.lineTo(e.clientX, e.clientY);
+    ctx.stroke();
+  }
+  
+  //event listeners to allow drawing when mouse is pressed down
+
+  canvas.addEventListener("mousedown", startPosition);
+  canvas.addEventListener("mouseup", finishedPosition);
+  canvas.addEventListener("mousemove", draw);
+});
 
